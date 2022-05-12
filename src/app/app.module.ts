6980a557efc43093fd3e017665ce402f5415ccbf
Roman Remorenko <Roman.fm@outlook.com>
2022-05-12 20:55:19 +0300

    feat: create translator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,56 @@
 import { NgModule } from '@angular/core';
-import { TuiRootModule } from "@taiga-ui/core";
+import {
+  TuiSvgModule,
+  TuiRootModule,
+  TuiHintModule,
+  TuiAlertModule,
+  TuiButtonModule,
+  TuiLoaderModule,
+  TuiDataListModule,
+  TuiTextfieldControllerModule,
+} from "@taiga-ui/core";
+import {
+  TuiIslandModule,
+  TuiSelectModule,
+  TuiTextAreaModule,
+  TuiLineClampModule,
+  TuiDataListWrapperModule,
+} from '@taiga-ui/kit';
+import { TuiLetModule } from '@taiga-ui/cdk';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { TranslatorComponent } from './components/translator/translator.component';
+import { TranslatorFormComponent } from './components/translator-form/translator-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatorComponent,
+    TranslatorFormComponent,
   ],
   imports: [
+    TuiLetModule,
+    TuiSvgModule,
     BrowserModule,
-    AppRoutingModule,
-      TuiRootModule,
-      BrowserAnimationsModule,
-],
+    TuiRootModule,
+    TuiHintModule,
+    TuiAlertModule,
+    TuiLoaderModule,
+    TuiButtonModule,
+    TuiSelectModule,
+    TuiIslandModule,
+    TuiTextAreaModule,
+    TuiDataListModule,
+    TuiLineClampModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    TuiDataListWrapperModule,
+    TuiTextfieldControllerModule,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/translator/translator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-translator',
+  templateUrl: './translator.component.html',
+  styleUrls: ['./translator.component.scss']
+})
+export class TranslatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
