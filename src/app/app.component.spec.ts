037ca3e96732c25cd092589982b9fb0a3dc68b1c
Roman Remorenko <Roman.fm@outlook.com>
2022-05-12 20:57:03 +0300

    feat: write app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,22 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+
+@Component({selector: 'tui-root', template: ''})
+class TuiRootStubComponent {}
+
+@Component({selector: 'mt-translator', template: ''})
+class TranslatorStubComponent {
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        TuiRootStubComponent,
+        TranslatorStubComponent,
       ],
     }).compileComponents();
   });
@@ -20,16 +27,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-translator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-translator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-translator app is running!');
-  });
 });
