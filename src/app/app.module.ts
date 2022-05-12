add18c4e397cb53680fe36a6ba34b7ecc11ffff2
Roman Remorenko <Roman.fm@outlook.com>
2022-05-12 20:51:07 +0300

    feat: add taiga-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { TuiRootModule } from "@taiga-ui/core";
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
